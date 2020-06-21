d8b5f538bfd8845c1331df8870d7f77a562eff62
Abigail Swigert <abigail@swigertdesigns.com>
2020-06-21 13:48:50 -0500

    completed cell phone exercise

--- udemy_exercises/cell_phone/CellPhone.java
+++ udemy_exercises/cell_phone/CellPhone.java
@@ -1,19 +1,101 @@
-package udemy_exercises.cell_phone;
+package cell_phone;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 class CellPhone {
+    private String myNumber;
+    private ArrayList<Contact> myContacts;
+    private static Scanner scanner = new Scanner(System.in);
 
+    public CellPhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<Contact>();
+    }
+
+    public boolean addContact(Contact contact) {
+        if(findContact(contact.getName()) >= 0) {
+            System.out.println("Contact is already on file");
+            return false;
+        }
+        myContacts.add(contact);
+        return true;
+    }
+
+    public void quit() {
+        System.out.println("\nShutting down...");
+    }
+
+    public boolean updateContact(Contact oldContact, Contact newContact) {
+        int foundPosition = findContact(oldContact);
+        if (foundPosition < 0) {
+            System.out.println(oldContact.getName() + " was not found.");
+            return false;
+        }
+        this.myContacts.set(foundPosition, newContact);
+        System.out.println(oldContact.getName() + " was replaced with " + newContact.getName());
+        return true;
+    }
+
+    public boolean removeContact(Contact contact) {
+        int foundPosition = findContact(contact);
+        if (foundPosition < 0) {
+            System.out.println(contact.getName() + " was not found.");
+            return false;
+        }
+        this.myContacts.remove(foundPosition);
+        System.out.println(contact.getName() + " was deleted.");
+        return true;
+    }
+
+    private int findContact(Contact contact) {
+        return this.myContacts.indexOf(contact);
+    }
+
+    private int findContact(String contactName) {
+        for(int i=0; i<this.myContacts.size(); i++) {
+            Contact contact = this.myContacts.get(i);
+            if(contact.getName().equals(contactName)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public String searchContact(Contact contact) {
+        if(findContact(contact) >= 0) {
+            return contact.getName();
+        }
+        return null;
+    }
+
+    public Contact searchContact(String name) {
+        int position = findContact(name);
+        if(position >= 0) {
+            return this.myContacts.get(position);
+        }
+        return null;
+    }
+
+    public void showContacts() {
+        System.out.println("Contact List");
+        for(int i=0; i<this.myContacts.size(); i++) {
+            System.out.println((i + 1) + "." + this.myContacts.get(i).getName() + " -> " +
+                                this.myContacts.get(i).getPhoneNumber());
+        }
+    }
 }
 
 /*
 **Cell Phone with Java Classes**
     - **CellPhone Class:**
-        - Contact List
-        - quit()
-        - showContacts()
-        - addContact()
-        - updateContat()
-        - removeContact()
-        - searchContact()
+        - Contact List -
+        - quit() -
+        - showContacts()-
+        - addContact() -
+        - updateContact() -
+        - removeContact() -
+        - searchContact() -
       * **When adding or updating be sure to check if the contact already exists**
       * **Be sure not to expose the inner workings of the Arraylist to MobilePhone (use concepts of encapsulation to meet this requirement)**
 */
