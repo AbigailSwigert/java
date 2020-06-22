c64282edb9ec99dd2d051ec5cafb58b6179fbb0d
Abigail Swigert <abigail@swigertdesigns.com>
2020-06-22 08:55:56 -0500

    completed the bank exercise

--- udemy_exercises/bank/Bank.java
+++ udemy_exercises/bank/Bank.java
@@ -1,7 +1,111 @@
-package udemy_exercises.bank;
+package bank;
+
+import java.util.ArrayList;
 
 public class Bank {
-    
+    private String name;
+    private ArrayList<Branch> branches;
+
+    public Bank(String name) {
+        this.name = name;
+        this.branches = new ArrayList<Branch>();
+    }
+
+    public boolean addBranch(String branchName) {
+        if(findBranch(branchName) == null) {
+            this.branches.add(new Branch(branchName));
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addCustomer(String branchName, String customerName, double initialAmount) {
+        Branch branch = findBranch(branchName);
+        if(branch != null) {
+            return branch.newCustomer(customerName, initialAmount);
+        }
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String branchName, String customerName, double amount) {
+        Branch branch = findBranch(branchName);
+        if(branch != null) {
+            return branch.addCustomerTransaction(customerName, amount);
+        }
+        return false;
+    }
+
+    private Branch findBranch(String branchName) {
+        for(int i=0; i<branches.size(); i++) {
+            Branch checkedBranch = this.branches.get(i);
+            if(checkedBranch.getName().equals(branchName)) {
+                return checkedBranch;
+            }
+        }
+        return null;
+    }
+
+    public boolean listCustomers(String branchName, boolean showTransactions) {
+        Branch branch = findBranch(branchName);
+        if(branch != null) {
+            System.out.println("Customer details for branch " + branch.getName() + ":");
+            ArrayList<Customer> branchCustomers = branch.getCustomers();
+            for( int i=0; i<branchCustomers.size(); i++) {
+                Customer branchCustomer = branchCustomers.get(i);
+                System.out.println("Customer: " + branchCustomer.getName() + "[" + (i + 1) + "]");
+                if (showTransactions) {
+                    System.out.println("Transactions: ");
+                    ArrayList<Double> transactions = branchCustomer.getTransactions();
+                    for(int j=0; j<transactions.size(); j++) {
+                        System.out.println("[" + (j + 1) + "] Amount: " + transactions.get(j));
+                    }
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Bank bank = new Bank("National Bank");
+
+        if(bank.addBranch("Dallas")) {
+            System.out.println("Dallas branch created");
+        }
+
+        bank.addCustomer("Dallas", "Abigail", 50.05);
+        bank.addCustomer("Dallas", "Taryn", 1200.87);
+        bank.addCustomer("Dallas", "Mike", 2.09);
+
+        bank.addBranch("Malibu");
+        bank.addCustomer("Malibu", "Bob", 198.72);
+
+        bank.addCustomerTransaction("Dallas", "Abigail", 44.22);
+        bank.addCustomerTransaction("Dallas", "Abigail", 109.86);
+        bank.addCustomerTransaction("Dallas", "Taryn", -90.72);
+
+        bank.listCustomers("Dallas", true);
+        bank.listCustomers("Malibu", true);
+
+        bank.addBranch("Columbus");
+
+        if(!bank.addCustomer("Columbus", "Dustin", 5.53)) {
+            System.out.println("Error Columbus branch does not exist");
+        }
+
+        if(!bank.addBranch("Dallas")) {
+            System.out.println("Dallas branch already exists");
+        }
+
+        if(!bank.addCustomerTransaction("Dallas", "Fergus", 52.33)) {
+            System.out.println("Customer does not exist at branch");
+        }
+
+        if(!bank.addCustomer("Dallas", "Abigail", 12.21)) {
+            System.out.println("Customer Abigail already exists");
+        }
+    }
 }
 
 
