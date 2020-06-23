598a0680399a7710f155c71fde363885d8a870c5
Abigail Swigert <abigail@swigertdesigns.com>
2020-06-23 16:01:55 -0500

    completed exercise 2 arrays

--- exercise2/Arrays.java
+++ exercise2/Arrays.java
@@ -1,14 +1,47 @@
-package exercise2;
+//const nums = [10,23,3,4,5,2,1];
+//const greetings = ["hello","howdy","hallo","hola","bonjour","ciao","أهلا"];
+//const binaries = [0,1,1,1,0,0,0,1,1,1,1,1,0,1,0,0,1,0];
+//nums.forEach(n => console.log(n));
+//greetings.forEach(g => console.log(g));
+//binaries.forEach(b => console.log(!!b));
+
+import java.util.ArrayList;
 
 class Arrays {
     //Take the arrays.js file, and refactor the code into Java in this file.
-    int[] nums;
-    String[] greetings;
-    boolean[] binaries;
+    static int[] nums = {10, 23, 3, 4, 5, 2, 1};
+    static String[] greetings = {"hello", "howdy", "hallo", "hola", "bonjour", "ciao", "أهلا"};
+    static int[] binaryInts = {0,1,1,1,0,0,0,1,1,1,1,1,0,1,0,0,1,0};
+    static ArrayList<Boolean> binaries = new ArrayList<Boolean>();
 
-    void printNums() {};
-    void printGreetings() {};
-    void printBinaries() {};
+    private static void printNums() {
+        for (int n: nums) {
+            System.out.println(n);
+        }
+    };
+    private static void printGreetings() {
+        for (String g: greetings) {
+            System.out.println(g);
+        }
+    };
+    private static void printBinaries() {
+        for (int i: binaryInts) {
+            if ( i == 0) {
+                binaries.add(false);
+            } else if ( i == 1) {
+                binaries.add(true);
+            }
+        }
+        for (boolean b: binaries) {
+            System.out.println(b);
+        }
+    };
+
+    public static void main(String[] args) {
+        printNums();
+        printGreetings();
+        printBinaries();
+    }
 }
 
 
